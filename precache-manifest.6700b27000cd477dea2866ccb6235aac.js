cd095e4f5fd670616fca53f44afc6f98719dbfa5
xiexingen <1002275364@qq.com>
2020-02-27 02:17:08 +0000

    Deploying to gh-pages from  - 085658b625e431046a7162339e4c09f7cb09cb21 🚀

---
+++ precache-manifest.6700b27000cd477dea2866ccb6235aac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7224405053366ad6f0b",
+    "url": "/wetrial-doc/umi.08f80b36.css"
+  },
+  {
+    "revision": "d7224405053366ad6f0b",
+    "url": "/wetrial-doc/umi.684448df.js"
+  },
+  {
+    "revision": "46ec5414c76f1e35da80d1bcaebd592b",
+    "url": "/wetrial-doc/index.html"
+  },
+  {
+    "revision": "cfeac0967a78fe9440872a7bbaaee1f0",
+    "url": "/wetrial-doc/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/wetrial-doc/pwacompat.min.js"
+  }
+];
